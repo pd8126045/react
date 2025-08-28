ccc5e4a224768a6812c692be92d9a3e4b56fd3f4
Priiyanka Das <pd8126045@gmail.com>
2025-08-28 23:20:53 +0530

    change some code

--- color-change/src/App.jsx
+++ color-change/src/App.jsx
@@ -4,77 +4,58 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-   const colorRed = () => {
-    document.querySelector('body').style.backgroundColor = 'red'
-
-   }
-   const colorWhite = () => {
-    document.querySelector('body').style.backgroundColor = 'white'
-
-   }
-   const colorGreen = () => {
-    document.querySelector('body').style.backgroundColor = 'green'
-
-   }
-   const colorLavender = () => {
-    document.querySelector('body').style.backgroundColor = '#E6E6FA'
-
-   }
-   const colorYellow = () => {
-    document.querySelector('body').style.backgroundColor = 'yellow'
-
-   }
-   const colorPink = () => {
-    document.querySelector('body').style.backgroundColor = 'pink'
-
-   }
-   const colorPurple = () => {
-    document.querySelector('body').style.backgroundColor = 'purple'
-
-   }
-   const colorBlue = () => {
-    document.querySelector('body').style.backgroundColor = 'blue'
-
-   }
+  const [color, setColor] = useState('black')
    
-   const colorGray = () => {
-    document.querySelector('body').style.backgroundColor = 'gray'
-
-   }
-   const colorBlack = () => {
-    document.querySelector('body').style.backgroundColor = 'black'
-
-   }
-   const colorOlive = () => {
-    document.querySelector('body').style.backgroundColor = '#808000'
-
-   }
-   const colorAqua = () => {
-    document.querySelector('body').style.backgroundColor = 'aqua'
-
-   }
+   
   
 
   return (
     <>
-      <div >
-        <button className='bg-red-700' onClick={colorRed}>Red</button>
-        <button className='bg-stone-400' onClick={colorWhite}>White</button>
-        <button className='bg-yellow-300' onClick={colorYellow}>Yellow</button>
-        <button className='bg-green-700' onClick={colorGreen}>Green</button>
-        <button className='bg-pink-400' onClick={colorPink}>Pink</button>
-        <button className='bg-purple-700' onClick={colorPurple}>Purple</button>
-        <button className='bg-blue-700' onClick={colorBlue}>Blue</button>
-        <button className='bg-violet-200' onClick={colorLavender}>Lavender</button>
-        <button className='bg-lime-700' onClick={colorOlive}>Olive</button>
-        <button className='bg-black' onClick={colorBlack}>Black</button>
-        <button className='bg-gray-700' onClick={colorGray}>Gray</button>
-        <button className='bg-sky-700' onClick={colorAqua}>Aqua</button>
+      <div className="w-full h-screen absolute top-0 left-0 " 
+      style={{ backgroundColor: color }}>
+
+        <div className='text-white'>
+        <button className='bg-red-700' 
+        onClick={() =>setColor("red")  } >Red</button>
+
+        <button className='bg-stone-400' 
+        onClick={() =>setColor("white")}>White</button>
+
+        <button className='bg-yellow-300'
+        onClick={() =>setColor("yellow")}>Yellow</button>
+
+        <button className='bg-green-700'
+        onClick={() =>setColor("green") }>Green</button>
+
+        <button className='bg-pink-400'
+        onClick={() =>setColor("pink")} >Pink</button>
+
+        <button className='bg-purple-700'
+        onClick={() =>setColor("purple")} >Purple</button>
+
+        <button className='bg-blue-700' 
+        onClick={() =>setColor("blue")}>Blue</button>
+        <button className='bg-violet-200'
+        onClick={() =>setColor("lavender")} >Lavender</button>
+
+        <button className='bg-lime-700' 
+        onClick={() =>setColor("olive")}>Olive</button>
+
+        <button className='bg-black' 
+        onClick={() =>setColor("black")}>Black</button>
+
+        <button className='bg-gray-700'
+        onClick={() =>setColor("gray")} >Gray</button>
+
+        <button className='bg-sky-700' 
+        onClick={() =>setColor("aqua")}>Aqua</button>
 
 
 
       </div>
+        
+      </div>
+      
     </>
   )
 }
