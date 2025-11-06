1326a0567b7d47b2fbead02a6f1169bbcac8654b
Priiyanka Das <pd8126045@gmail.com>
2025-11-06 21:27:28 +0530

    Errorthrow

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,15 +1,68 @@
 
-import Create from "./try.jsx"
+import React from "react";
+import { useEffect, useState } from 'react'
 function App() {
   const userName = "chai or code"
   
 
   return (
     <>
-    <Create/>
+    <ErrorBoundary>
+      <Card1/>
+    </ErrorBoundary>
+    <Card2/>
     </>
     
   )
 }
+function Card1(){
+  throw new Error("Error")
+  return <div style = {{backgroundColor:"red",padding:15,borderRadius:5, margin:10}}>
+    hi there
+  </div>
+
+}
+function Card2(){
+  //throw new Error("Error")
+  return <div style ={{backgroundColor:"red",padding:15,borderRadius:5, margin:10}}>
+    hello
+  </div>
+
+  
+}
+
+
+
+
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render shows the fallback UI
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    // You can log the error to an error reporting service here
+    console.error("Error caught by ErrorBoundary:", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Render fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+    // Otherwise, render children components normally
+    return this.props.children;
+  }
+}
+
+
+
+
 
 export default App
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -6,34 +6,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 
-/*const reactElement = {
-    type: "a",
-    props: {
-        href: "https://goole.com",
-        target: "_blank"
-    },
-    children : "click me to visit google",
-  }*/
-  const anotherUser = " try to understander"
-  const reactElement = React.createElement(
-      "a",
-      {href : "https://goole.com",target : "_blank"},
-      "click me to visit google",
-      anotherUser
-    )
 
-    
-
-  const anotherElement = (
-    <a href="https://goole.com" target='_blank'>visit goole</a>
-  )
-
+  
   
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-
-    {reactElement}
-  </StrictMode>
-
+    <App/>
 )
